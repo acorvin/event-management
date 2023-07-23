ef43368da61ac0d5fe3cc262c2a7ea3766a66630
Alex Corvin <corvindesigns@gmail.com>
2023-07-23 10:31:52 -0600

    configuring email notifications for upcoming events

--- app/Console/Commands/SendEventReminders.php
+++ app/Console/Commands/SendEventReminders.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Event;
+use App\Notifications\EventReminderNotification;
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
 
@@ -36,7 +37,11 @@ class SendEventReminders extends Command
 
         $events->each(
             fn ($event) => $event->attendees->each(
-                fn ($attendee) => $this->info("Notifying the user {$attendee->user->id}")));
+                fn ($attendee) => $attendee->user->notify(
+                    new EventReminderNotification($event)
+                )
+            )
+        );
 
         $this->info("Found {$eventCount} {$eventLabel}.");
         $this->info('Reminder notifications sent successfully!');
