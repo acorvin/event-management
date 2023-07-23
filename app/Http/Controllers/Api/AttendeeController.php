59403a75d4c68c6e2963186055eebb0febd472e0
Alex Corvin <corvindesigns@gmail.com>
2023-07-23 10:51:42 -0600

    configuring rate limiting for the api

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -17,8 +17,8 @@ class AttendeeController extends Controller
     public function __construct()
     {
         $this->middleware('auth:sanctum')->except(['index', 'show', 'update']);
+        $this->middleware('throttle:api')->only(['store', 'destroy']);
         $this->authorizeResource(Attendee::class, 'attendee');
-
     }
 
     /**
@@ -41,7 +41,7 @@ class AttendeeController extends Controller
     {
         $attendee = $this->loadRelationships(
             $event->attendees()->create([
-            'user_id' => 1
+            'user_id' => $request->user()->id
         ]));
 
         return new AttendeeResource($attendee);
@@ -68,7 +68,6 @@ class AttendeeController extends Controller
      */
     public function destroy(Event $event, Attendee $attendee)
     {
-        // $this->authorize('delete-attendee', [$event, $attendee]);
         $attendee->delete();
 
         return response(status: 204);
